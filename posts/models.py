f33bde2c6f6f8b08b431bfbbd6abae8d0c535d07
Hung Ta <hung.ta@eastagile.com>
2024-08-31 17:41:01 +0700

    Display post

--- posts/models.py
+++ posts/models.py
@@ -5,8 +5,11 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-  id = models.UUIDField(default= uuid4(), primary_key=True)
-  title = models.CharField(max_length=255)
-  image = models.URLField()
-  body = models.CharField(max_length=255)
+  id = models.UUIDField(default= uuid4(), primary_key=True, unique=True, editable=True)
+  title = models.CharField(max_length=500)
+  image = models.URLField(max_length=512)
+  body = models.TextField()
   created = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self) -> str:
+    return str(self.title)
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
 
 def home_view(request):
-  return render(request, 'index.html')
+  posts = Post.objects.all()
+
+  return render(request, 'index.html', {"posts": posts})
