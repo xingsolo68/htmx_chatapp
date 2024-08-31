0165cada3e5a902b9ffdb64d9182a574e402e88d
Hung Ta <hung.ta@eastagile.com>
2024-08-31 18:58:54 +0700

    Add fields to post model

--- posts/models.py
+++ posts/models.py
@@ -6,10 +6,15 @@ from django.db import models
 
 class Post(models.Model):
   id = models.UUIDField(default= uuid4(), primary_key=True, unique=True, editable=True)
+  artist = models.CharField(max_length=500, null=True)
   title = models.CharField(max_length=500)
   image = models.URLField(max_length=512)
+  url = models.URLField(max_length=512, null=True)
   body = models.TextField()
   created = models.DateTimeField(auto_now_add=True)
 
   def __str__(self) -> str:
     return str(self.title)
+
+  class Meta:
+    ordering = ['-created']
