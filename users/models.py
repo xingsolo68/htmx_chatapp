80b856491b76b09d7f3a10b223fb135cd47210e9
Hung Ta <hung.ta@eastagile.com>
2024-09-02 20:59:15 +0700

    Add profile model

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="avatars", null=True, blank=True)
+    first_name = models.CharField(max_length=20, null=True, blank=True)
+    last_name = models.CharField(max_length=20, null=True, blank=True)
+    email = models.EmailField(unique=True, null=True)
+    location = models.CharField(max_length=20, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
